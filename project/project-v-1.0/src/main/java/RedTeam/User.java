63a90eefca270f3aebd16e4472c4f35914cd83aa
jmcaplan <54451212+jmcaplan@users.noreply.github.com>
2020-07-01 18:35:46 -0400

    add basic user class with constructor

--- project/project-v-1.0/src/main/java/RedTeam/User.java
+++ project/project-v-1.0/src/main/java/RedTeam/User.java
@@ -1,3 +1,21 @@
+package RedTeam;
+
+import java.util.UUID;
+
 public abstract class User {
     
+    String firstName;
+    String middleName;
+    String lastName;
+    String email;
+    String password;
+
+    public User(String fullName, String email, String password) {
+        String[] names = fullName.split(" ");
+        this.firstName = names[0];
+        this.middleName = (names.length > 2) ? names[1] : "";
+        this.lastName = names[names.length-1];
+    }
+
+
 }
