3e9cf69a92748473f685a663ba0047d50e3ce5cb
jmcaplan <54451212+jmcaplan@users.noreply.github.com>
2020-07-01 18:41:16 -0400

    added request queue in user object

--- project/project-v-1.0/src/main/java/RedTeam/User.java
+++ project/project-v-1.0/src/main/java/RedTeam/User.java
@@ -1,5 +1,6 @@
 package RedTeam;
 
+import java.util.Queue;
 import java.util.UUID;
 
 public abstract class User {
@@ -9,13 +10,18 @@ public abstract class User {
     String lastName;
     String email;
     String password;
+    Queue<Form> requestQueue;
 
     public User(String fullName, String email, String password) {
         String[] names = fullName.split(" ");
         this.firstName = names[0];
         this.middleName = (names.length > 2) ? names[1] : "";
         this.lastName = names[names.length-1];
+        this.requestQueue = new Queue<Form>();
     }
 
+    public Queue<Form> getAllForms() {
+        return this.requestQueue;
+    }
 
 }
