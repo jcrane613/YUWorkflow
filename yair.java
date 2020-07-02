c94e930228b855189c2176b9c874f34b6ffe6fae
jhaller19 <jhaller0899@gmail.com>
2020-07-02 13:39:01 -0400

    init

--- yair.java
+++ yair.java
@@ -1,6 +1,6 @@
 public class yair{
     public static void main(String[] args) {
         System.out.println("Hello Jonathan!"); 
-        System.out.println("Hello Crane!");
+        System.out.println("א");
     }
 }
