6c3eaeaf684676a4ad316df22ecf8bd3d653ae3f
jmcaplan <54451212+jmcaplan@users.noreply.github.com>
2020-07-24 12:26:46 -0400

    Improved UI for admin dashboard

--- project/spring-boot-shopping-cart-master/src/main/java/com/reljicd/ShoppingCartApplication.java
+++ project/spring-boot-shopping-cart-master/src/main/java/com/reljicd/ShoppingCartApplication.java
@@ -6,13 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-
-import com.reljicd.model.Form;
-import com.reljicd.service.FormService;
-import com.reljicd.service.impl.EmailServiceImpl;
 
 @SpringBootApplication
 public class ShoppingCartApplication {
